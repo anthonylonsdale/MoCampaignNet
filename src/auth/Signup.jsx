7aa2aa1c9e7a8111a35411c1ed13fda538926e34
anthonylonsdale <alons3253@gmail.com>
2023-11-11 14:43:16 -0600

    auth screen design changes

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -5,10 +5,11 @@ import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase
 import { collection, doc, getDocs, query, setDoc, where } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import logo from '../images/logoblank.png'
 import './Signup.css'
 import { db } from './firebase.jsx'
 
-const { Text, Title } = Typography
+const { Text } = Typography
 
 function Signup() {
   const [error, setError] = useState('')
@@ -117,82 +118,96 @@ function Signup() {
   }, [form, newPassword, displayName])
 
   return (
-    <div className="signup-container">
-      <Title level={2}>Sign Up</Title>
-      <Form name="signup" onFinish={onFinish} layout="vertical" form={form}>
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              type: 'email',
-              message: 'Please enter a valid email address!',
-            },
-          ]}
+    <div className="auth-page">
+      <div className="auth-header">
+        <img src={logo} alt="Bernoulli Technologies Logo" className="auth-logo"/>
+        <span className="auth-title">Account Signup</span>
+      </div>
+      <div className="auth-container">
+        <Form
+          name="signup"
+          onFinish={onFinish}
+          layout="vertical"
+          form={form}
+          className="auth-form"
         >
-          <Input placeholder="Email" />
-        </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            className="auth-form-item"
+            rules={[
+              { required: true, type: 'email', message: 'Please enter a valid email address!' },
+            ]}
+          >
+            <Input placeholder="Email" className="auth-input" />
+          </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please enter your password!',
-            },
-          ]}
-        >
-          <Input.Password
-            placeholder="Password"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-            onFocus={onFocusPassword}
-            onBlur={onBlurPassword}
-          />
-        </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            className="auth-form-item"
+            rules={[
+              { required: true, message: 'Please enter your password!' },
+            ]}
+          >
+            <Input.Password
+              placeholder="Password"
+              className="auth-input"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              onFocus={onFocusPassword}
+              onBlur={onBlurPassword}
+            />
+          </Form.Item>
 
-        <Form.Item
-          label="Display Name"
-          name="displayName"
-          rules={[
-            {
-              required: true,
-              message: 'Please enter your display name!',
-            },
-          ]}
-        >
-          <Input
-            placeholder="Display Name"
-            value={displayName}
-            onChange={(e) => setDisplayName(e.target.value)}
-          />
-        </Form.Item>
-        {showRequirements && (
-          <List
-            dataSource={requirementsData.map((requirement, index) => ({
-              ...requirement,
-              icon: generateIcon(passwordRequirements[index]),
-            }))}
-            renderItem={(item) => (
-              <List.Item>
-                {item.icon}&nbsp;{item.text}
-              </List.Item>
-            )}
-          />
-        )}
-        {error && <div className="error-text">{error}</div>}
-        <Form.Item>
-          <Button type="primary" htmlType="submit" disabled={isSubmitDisabled}>
+          <Form.Item
+            label="Display Name"
+            name="displayName"
+            className="auth-form-item" // Assuming this class is styled in Signup.css
+            rules={[
+              { required: true, message: 'Please enter your display name!' },
+            ]}
+          >
+            <Input
+              placeholder="Display Name"
+              className="auth-input"
+              value={displayName}
+              onChange={(e) => setDisplayName(e.target.value)}
+            />
+          </Form.Item>
+          {showRequirements && (
+            <div className="password-requirements">
+              <List
+                size="small"
+                bordered
+                dataSource={requirementsData.map((requirement, index) => ({
+                  ...requirement,
+                  icon: generateIcon(passwordRequirements[index]),
+                }))}
+                renderItem={(item) => (
+                  <List.Item>
+                    {item.icon}&nbsp;{item.text}
+                  </List.Item>
+                )}
+              />
+            </div>
+          )}
+          {error && <div className="error-text">{error}</div>}
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              disabled={isSubmitDisabled}
+              className="auth-submit-button"
+            >
             Sign Up
-          </Button>
-        </Form.Item>
-      </Form>
-      <Text>
-        Already have an account? <Link to="/login"
-          className="auth-link">Sign In</Link>
-      </Text>
+            </Button>
+          </Form.Item>
+        </Form>
+        <Text className="auth-footer-text"> {/* New class for footer text */}
+        Already have an account? <Link to="/login" className="auth-link">Sign In</Link>
+        </Text>
+      </div>
     </div>
   )
 }
--- src/auth/auth.jsx
+++ src/auth/auth.jsx
@@ -1,8 +1,8 @@
-import { Button, Form, Image, Input, Typography } from 'antd'
+import { Button, Form, Input, Typography } from 'antd'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import logo from '../images/logo.png'
+import logo from '../images/logoblank.png'
 import { loginTracer } from './LoginTracer.jsx'
 import './auth.css'
 
@@ -17,9 +17,8 @@ function Auth() {
     const auth = getAuth()
     try {
       await signInWithEmailAndPassword(auth, email, password)
-      console.log(auth)
-      loginTracer(auth.currentUser.uid)
 
+      loginTracer(auth.currentUser.uid)
       history('/campaign-tools')
     } catch (error) {
       setError(error.message)
@@ -27,50 +26,55 @@ function Auth() {
   }
 
   return (
-    <div className="auth-container">
-      <Image src={logo} alt="Your Logo" className="logo" preview={false} />
-      <Form
-        name="signin"
-        onFinish={onFinish}
-        layout="vertical"
-      >
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              type: 'email',
-              message: 'Please enter a valid email address!',
-            },
-          ]}
+    <div className="auth-page">
+      <div className="auth-header">
+        <img src={logo} alt="Bernoulli Technologies Logo" className="auth-logo"/>
+        <span className="auth-title">Account Login</span>
+      </div>
+      <div className="auth-container">
+        <Form
+          name="signin"
+          onFinish={onFinish}
+          layout="vertical"
         >
-          <Input autoComplete="on" name='email' />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please enter your password!',
-            },
-          ]}
-        >
-          <Input.Password autoComplete="" name='password' />
-        </Form.Item>
-        {error && <div className="error">{error}</div>}
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                type: 'email',
+                message: 'Please enter a valid email address!',
+              },
+            ]}
+          >
+            <Input autoComplete="on" name='email' />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter your password!',
+              },
+            ]}
+          >
+            <Input.Password autoComplete="" name='password' />
+          </Form.Item>
+          {error && <div className="error">{error}</div>}
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
             Sign In
-          </Button>
-        </Form.Item>
-      </Form>
-      <Text>
-        Don&apos;t have an account? <Link to="/signup"
-          className="auth-link">Sign Up</Link>
-      </Text>
+            </Button>
+          </Form.Item>
+        </Form>
+        <Text>
+        Don&apos;t have an account? <Link to="/signup" className="auth-link">Sign Up</Link>
+        </Text>
+      </div>
     </div>
+
   )
 }
 
