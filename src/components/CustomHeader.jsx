f0812e64e3b6be917b698b03cc8fb61e57f2261f
WebsiteDeployer <respected_corporate_analyst@protonmail.com>
2025-09-05 20:12:01 -0500

    test

--- src/components/CustomHeader.jsx
+++ src/components/CustomHeader.jsx
@@ -1,8 +1,4 @@
-import {
-  HomeOutlined,
-  SettingOutlined,
-  UserOutlined,
-} from '@ant-design/icons'
+import { HomeOutlined, SettingOutlined } from '@ant-design/icons'
 import { Image, Layout, Menu } from 'antd'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
@@ -19,6 +15,8 @@ function CustomHeader() {
     const currentScrollPos = window.scrollY
     if (currentScrollPos > lastScrollTop && currentScrollPos > 0) {
       setVisible(false)
+    } else {
+      setVisible(true)
     }
     lastScrollTop = currentScrollPos <= 0 ? 0 : currentScrollPos
   }
@@ -34,7 +32,6 @@ function CustomHeader() {
   useEffect(() => {
     const debouncedHandleScroll = debounce(handleScroll)
     window.addEventListener('scroll', debouncedHandleScroll)
-
     return () => {
       window.removeEventListener('scroll', debouncedHandleScroll)
     }
@@ -44,19 +41,21 @@ function CustomHeader() {
     <Header className={`site-layout-background ${!visible && 'header-hidden'}`}>
       <Link to="/" className="logo-link">
         <Image src={logo} alt="Logo" className="header-logo" preview={false} />
+        <div className='header-title'>Bernoulli Technologies</div>
       </Link>
-      <div className='header-title'>Bernoulli Technologies</div>
       <div className="header-right">
         <Menu theme="dark" mode="horizontal" className="disable-select custom-menu">
           <Menu.Item key="1" className="custom-menu-item" icon={<HomeOutlined />}>
             <Link to="/">Home</Link>
           </Menu.Item>
+          {/*
           <Menu.Item key="2" className="custom-menu-item" icon={<UserOutlined />}>
             <Link to="/portfolio">Portfolio</Link>
           </Menu.Item>
           <Menu.Item key="3" className="custom-menu-item" icon={<SettingOutlined />}>
             <Link to="/campaign-tools">Campaign Tools</Link>
           </Menu.Item>
+          */}
         </Menu>
       </div>
     </Header>
